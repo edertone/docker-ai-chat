03bf38ef35801fe2e926fec3578505323accda6f
Jaume Mussons <jaume.mussons@edertone.com>
2026-04-04 23:59:39 +0200

    Updated  models to the newest gemma 4 family

--- docker/init-models.sh
+++ docker/init-models.sh
@@ -28,36 +28,32 @@ pull_model "nomic-embed-text"
 # The smallest Llama 3.2 model, optimized for maximum speed and minimal VRAM usage. Great for simple tasks
 pull_model "llama3.2:1b"
 
-# Llama 3.2 (3 Billion parameters)
-# Highly optimized for speed and maximum VRAM efficiency. Perfect for simple tasks or huge context windows, though less capable at deep logic.
-pull_model "llama3.2:3b"
+# Gemma 4 (2 Billion parameters)
+# Google's latest model, offering a great balance of intelligence and speed. Requires ~1.5GB on disk and 2-4GB VRAM for optimal performance.
+pull_model "gemma4:e2b"
 
-# Llama 3.1 (8 Billion parameters)
-#An excellent all-around model offering a great balance of logic, coding ability, and speed. Fits well entirely inside an 8GB VRAM GPU.
-pull_model "llama3.1:8b"
+# Gemma 4 (4 Billion parameters)
+# Google's latest model, offering a great balance of intelligence and speed. Requires ~2.5GB on disk and 4-6GB VRAM for optimal performance.
+pull_model "gemma4:e4b"
 
 # DeepSeek-R1 (8 Billion parameters)
 # A powerful "reasoning" model that supports deep thinking processes. Requires ~4.7GB on disk and 6-8GB VRAM for smooth generation.
 # Excellent for complex logic, coding, and step-by-step problem solving.
-pull_model "deepseek-r1:8b"
-
-# Gemma 3 (12 Billion parameters)
-# Bleeding-edge generation from Google. Fills almost exactly 8GB of VRAM. Slower context processing but massive intelligence for its size.
-pull_model "gemma3:12b"
+# pull_model "deepseek-r1:8b"
 
 # DeepSeek-Coder V2 (16 Billion parameters)
 # A powerful coding model designed for complex programming tasks. Requires ~9GB on disk and 12-16GB VRAM for optimal performance.
 # Excellent for code generation, debugging, and understanding complex codebases.
 # pull_model "deepseek-coder-v2:16b"
 
-# this model is very large and will only work with 16GB vram GPUs, so it's commented out by default. Uncomment if you want to pull it.
-# gpt-oss:20b
+# gpt-oss:20b (20 Billion parameters)
 # OpenAI’s open-weight models designed for powerful reasoning, agentic tasks, and versatile developer use cases.
+# this model is very large and will only work with 16GB vram GPUs, so it's commented out by default. Uncomment if you want to pull it.
 # pull_model "gpt-oss:20b"
 
+# Gemma 4 (31 Billion parameters)
+# Google's latest model, offering a great balance of intelligence and speed. Requires ~20GB on disk and 24GB VRAM for optimal performance.
 # this model is very large and will only work with 24GB vram GPUs, so it's commented out by default. Uncomment if you want to pull it.
-# gemma3:27b
-# The current, most capable model that runs on a single GPU
-# pull_model "gemma3:27b"
+# pull_model "gemma4:31b"
 
 echo "All models ready."
